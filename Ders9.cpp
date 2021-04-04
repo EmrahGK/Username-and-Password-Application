ac984a97960faa075ddff788ad5c93d55753f9e8
Emrah Güvenkaya <guvenkayaemrah52@gmail.com>
2021-04-04 13:02:29 +0300

    Proje dosyası ekle.

---
+++ Ders9.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+   
+	string sys_username = "EmrahGK";
+	string sys_password = "password_of_app";
+
+	string username;
+	string password;
+
+	cout << "Please enter username: ";
+		cin >> username;
+	
+	cout << "Please enter password: ";
+		cin >> password;
+
+
+		if (sys_username == username && sys_password == password) {
+			cout << "Welcome!!";
+		}
+
+		else if (sys_username != username && sys_password == password) {
+			cout << "You entered username incorrectly. Please try again...";
+		}
+
+
+		else if (sys_username == username && sys_password != password) {
+			cout << "You entered password incorrectly. Please try again...";
+		}
+
+		else {
+			cout << "You entered username and password incorrectly. Please try again...";
+		}
+
+	return 0;
+
+}
